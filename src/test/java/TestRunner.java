695c8ed30c8e20d4528a2ad5bb19dd5790e71eac
Shareef SK <skshareef9133@gmail.com>
2025-07-18 23:17:51 +0530

    Delete src/test/java/TestRunner.java

--- src/test/java/TestRunner.java
+++
@@ -1,9 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "src/test/resources/features")
-
-public class TestRunner extends AbstractTestNGCucumberTests{
-
-}
-
